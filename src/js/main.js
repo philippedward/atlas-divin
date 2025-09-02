31de201b24563dfdc0097fca820f3fa8620b2f55
philippedward <amelie.rakowiecki@studentfr.ch>
2025-09-02 17:16:25 +0200

    switch mode complet

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+document.querySelector("html").setAttribute("data-theme", "light");
+
+const toggleDarkModeBtn = document.querySelector("#dark-mode-btn");
+const toggleLightModeBtn = document.querySelector("#light-mode-btn");
+
+toggleDarkModeBtn.addEventListener("click", function () {
+  document.querySelector("html").setAttribute("data-theme", "dark");
+});
+
+toggleLightModeBtn.addEventListener("click", function () {
+  document.querySelector("html").setAttribute("data-theme", "light");
+});
